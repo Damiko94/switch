104e0d991a1813cc96fa60c7e074f698c147d4b2
damien <damien.greuzat@sfr.fr>
2020-03-08 18:05:32 +0100

    <Mod>ajout infos pages admin

--- admin/gestion_admin.php
+++ admin/gestion_admin.php
@@ -6,12 +6,32 @@ if (!user_is_admin()) {
     header('location:../connexion.php');
     exit();
 }
+// differentes requetes pour affichage infos administrateur
+
+// requete pour le nombres de commandes
+$nb_commande = $pdo->query("SELECT id_commande FROM commande");
+$nb_c = $nb_commande->rowCount();
+
+// requete pour le nombre de clients
+$nb_clients = $pdo->query("SELECT id_membre FROM membre");
+$client = $nb_clients->rowCount();
+
+// requete pour les produits
+$produits = $pdo->query("SELECT id_produit FROM produit");
+$nb_produits = $produits->rowCount();
+$lb_produits = $pdo->query("SELECT id_produit FROM produit WHERE etat = 'libre'");
+$nb_lb_prod = $lb_produits->rowCount();
+
+// requete pour les salles
+$nb_salles = $pdo->query("SELECT titre FROM salle");
+$salle = $nb_salles->rowCount();
 
 include '../inc/header.inc.php';
 include '../inc/nav.inc.php';
 if (isset($_SESSION)) {
     // vd($_SESSION);
 }
+
 ?>
     <div class="row">
         <aside class="col-2">
@@ -34,6 +54,26 @@ if (isset($_SESSION)) {
             </ul>
         </aside>
         <section class="col-10">
+            <div class="row">
+                <div class="col-3">
+                    <h3>Commandes :</h3>
+                    <p>Vous avez actuellement <b><?= $nb_c ?></b> commande(s) en cours.</p>
+                </div>
+                <div class="col-3">
+                    <h3>Clients</h3>
+                    <p>Il y a actuellement <b><?= $client ?></b> compte(s) client(s).</p>
+                </div>
+                <div class="col-3">
+                    <h3>Produits</h3>
+                    <p>Il y a actuellement <b><?= $nb_produits ?></b> produits<br>
+                        dont <b><?= $nb_lb_prod ?></b> de libres.
+                    </p>
+                </div>
+                <div class="col-3">
+                    <h3>Salles</h3>
+                    <p>Switch possedent actuellement <b><?= $salle  ?></b> salles.</p>
+                </div>
+            </divrow>
         </section>
     </div>
 <?php

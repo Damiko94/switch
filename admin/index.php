89b91dfa3cfd56e1692873892560f3baec9819d2
damien <damien.greuzat@sfr.fr>
2020-03-10 23:55:06 +0100

    <Mod>affichage var_dump retirer sur la page profil

---
+++ admin/index.php
@@ -0,0 +1,16 @@
+<?php
+include '../inc/init.inc.php';
+include  '../inc/fonction.inc.php';
+
+
+include  '../inc/header.inc.php';
+include  '../inc/nav.inc.php';
+
+?>
+
+    <section style="height: 500px;">
+        <h1 style="color: red;">Bien tenté, essaye encore</h1>
+    </section>
+<?php
+
+include '../inc/footer.inc.php';
